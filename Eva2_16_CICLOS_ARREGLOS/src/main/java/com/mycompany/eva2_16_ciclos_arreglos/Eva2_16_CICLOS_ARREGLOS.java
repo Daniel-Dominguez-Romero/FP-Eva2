ea34434f5976940a5da121ab094a6d93babba3bb
Daniel-Dominguez-Romero <L24550320@chihuahua2.tecnm.mx>
2024-10-21 17:48:23 -0600

    Add files via upload

---
+++ Eva2_16_CICLOS_ARREGLOS/src/main/java/com/mycompany/eva2_16_ciclos_arreglos/Eva2_16_CICLOS_ARREGLOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_ciclos_arreglos;
+
+/**
+ *
+ * @author danie
+ */
+public class Eva2_16_CICLOS_ARREGLOS {
+
+    public static void main(String[] args) {
+        
+        int[] datos, x, y, z;//TODOS OSN ARREGLOS 
+        int val1, val2, arreglo[];//SOLO arreglo es ARREGLO 
+        
+        arreglo = new int[10]; 
+        
+        for (int i = 0; i < 10; i++) {
+            arreglo[i] = (int)(Math.random() * 100); 
+        }
+                
+        
+         for (int i = 0; i < 10; i++) {
+             System.out.println(arreglo[i]);
+         }
+        
+        
+        
+        
+        
+    }
+}
